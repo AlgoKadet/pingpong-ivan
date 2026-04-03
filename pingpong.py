4e0ddce6ad208438c989be1ef25df2879566fc44
Vanya <legoblue6@gmail.com>
2026-04-03 13:32:41 +0500

    123

--- pingpong.py
+++ pingpong.py
@@ -1,31 +1,13 @@
 from pygame import *
 from random import randint
-# подгружаем отдельно функции для работы со шрифтом
+
 font.init()
 font1 = font.Font(None, 80)
-win = font1.render('YOU WIN!', True, (255, 255, 255))
-lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+win1 = font1.render('RAKET 1 WIN!', True, (255, 255, 255))
+win2 = font1.render('RAKET 2 WIN!', True, (255, 255, 255))
 
-font2 = font.Font(None, 36)
 
-#фоновая музыка
-mixer.init()
-mixer.music.load('space.ogg')
-mixer.music.play()
-fire_sound = mixer.Sound('fire.ogg')
 
-# нам нужны такие картинки:
-img_back = "galaxy.jpg" # фон игры
- 
-img_bullet = "bullet.png" # пуля
-img_hero = "rocket.png" # герой
-img_enemy = "ufo.png" # враг
- 
-score = 0 # сбито кораблей
-goal = 10 # столько кораблей нужно сбить для победы
-lost = 0 # пропущено кораблей
-max_lost = 3 # проиграли, если пропустили столько
- 
 # класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
   # конструктор класса
@@ -49,58 +31,42 @@ class GameSprite(sprite.Sprite):
 # класс главного игрока
 class Player(GameSprite):
     # метод для управления спрайтом стрелками клавиатуры
-    def update(self):
+    def update_left(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_W] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_S] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
+    def update_right(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
   # метод "выстрел" (используем место игрока, чтобы создать там пулю)
     def fire(self):
         bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
         bullets.add(bullet)
 
 # класс спрайта-врага   
-class Enemy(GameSprite):
+class Ball(GameSprite):
     # движение врага
-    def update(self):
-        self.rect.y += self.speed
-        global lost
-        # исчезает, если дойдет до края экрана
-        if self.rect.y > win_height:
-            self.rect.x = randint(80, win_width - 80)
-            self.rect.y = 0
-            lost = lost + 1
- 
-# класс спрайта-пули   
-class Bullet(GameSprite):
-    # движение врага
-    def update(self):
-        self.rect.y += self.speed
-        # исчезает, если дойдет до края экрана
-        if self.rect.y < 0:
-            self.kill()
- 
-# Создаем окошко
-win_width = 700
-win_height = 500
-display.set_caption("Shooter")
-window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load(img_back), (win_width, win_height))
+    #def update(self):
+   #     self.rect.y += self.speed
+    #    if self.rect.x - raket_1 or raket_2 == 20:
+   #         pass
+#
  
 # создаем спрайты
-ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+raket_1 = Player('table-tennis-racket-and-ball-clipart-design-illustration-free-png.png', 50, 250, 75, 75, 10)
+raket_2 = Player('table-tennis-racket-and-ball-clipart-design-illustration-free-png.png', 450, 250, 75, 75, 10)
+ball = Ball('1_1735.png', 250, 250, 40, 40, 7)
+
+display.set_caption("Ping Pong")
+window = display.set_mode((win_width, win_height))
+background = transform.scale((500, 500))
+window.fill((0,0,0))
  
-# создание группы спрайтов-врагов
-monsters = sprite.Group()
-for i in range(1, 6):
-    monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-    monsters.add(monster)
- 
-bullets = sprite.Group()
- 
-# переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
-finish = False
 # Основной цикл игры:
 run = True # флаг сбрасывается кнопкой закрытия окна
 while run:
@@ -108,55 +74,32 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-        # событие нажатия на пробел - спрайт стреляет
-        elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
-                fire_sound.play()
-                ship.fire()
+        
  
   # сама игра: действия спрайтов, проверка правил игры, перерисовка
     if not finish:
         # обновляем фон
         window.blit(background,(0,0))
 
-        # пишем текст на экране
-        text = font2.render("Счет: " + str(score), 1, (255, 255, 255))
-        window.blit(text, (10, 20))
-
-        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
-        window.blit(text_lose, (10, 50))
-
         # производим движения спрайтов
-        ship.update()
-        monsters.update()
-        bullets.update()
+        raket_1.update()
+        raket_2.update()
+        ball.update()
 
         # обновляем их в новом местоположении при каждой итерации цикла
-        ship.reset()
-        monsters.draw(window)
-        bullets.draw(window)
+        raket_1.reset()
+        raket_2.reset()
+        ball.reset()
  
         # проверка столкновения пули и монстров (и монстр, и пуля при касании исчезают)
-        collides = sprite.groupcollide(monsters, bullets, True, True)
-        for c in collides:
-            # этот цикл повторится столько раз, сколько монстров подбито
-            score = score + 1
-            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
-            monsters.add(monster)
-
-        # возможный проигрыш: пропустили слишком много или герой столкнулся с врагом
-        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
-            finish = True # проиграли, ставим фон и больше не управляем спрайтами.
-            window.blit(lose, (200, 200))
-
-        # проверка выигрыша: сколько очков набрали?
-        if score >= goal:
-            finish = True
-            window.blit(win, (200, 200))
+       if ball.rect.x == 20:
+            window.blit(win2, (200, 200))
+        if ball.rect.x == 480:
+            window.blit(win1, (200, 200))
 
         display.update()
     # цикл срабатывает каждую 0.05 секунд
-    time.delay(50)
+    time.delay(60)
 
     #1111111111
     
