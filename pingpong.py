368f0c9284657c8d51f81195d40b375b8ec47d86
AlgoKadet <130219573+AlgoKadet@users.noreply.github.com>
2026-04-05 17:39:31 +0500

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -1,106 +1,99 @@
 from pygame import *
-from random import randint
+'''Необходимые классы'''
 
-font.init()
-font1 = font.Font(None, 80)
-win1 = font1.render('RAKET 1 WIN!', True, (255, 255, 255))
-win2 = font1.render('RAKET 2 WIN!', True, (255, 255, 255))
-
-
-
-# класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
-  # конструктор класса
-    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        # Вызываем конструктор класса (Sprite):
-        sprite.Sprite.__init__(self)
-
-        # каждый спрайт должен хранить свойство image - изображение
-        self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
-
-        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+    def __init__(self, sprite_image, pos_x, pos_y, speed, wight, hight):
+        super().__init__()
+        self.image = transform.scale(image.load(sprite_image), (wight, hight))
+        self.speed = speed
         self.rect = self.image.get_rect()
-        self.rect.x = player_x
-        self.rect.y = player_y
- 
-  # метод, отрисовывающий героя на окне
+        self.rect.x = pos_x
+        self.rect.y = pos_y
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
-# класс главного игрока
+
 class Player(GameSprite):
-    # метод для управления спрайтом стрелками клавиатуры
-    def update_left(self):
-        keys = key.get_pressed()
-        if keys[K_W] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_S] and self.rect.y < 500 - 80:
-            self.rect.y += self.speed
-    def update_right(self):
+    def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 500 - 80:
+        if keys[K_DOWN] and self.rect.y < win_height-80:
             self.rect.y += self.speed
-  # метод "выстрел" (используем место игрока, чтобы создать там пулю)
-    def fire(self):
-        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
-        bullets.add(bullet)
 
-# класс спрайта-врага   
-class Ball(GameSprite):
-    pass
-    # движение врага
-    #def update(self):
-   #     self.rect.y += self.speed
-    #    if self.rect.x - raket_1 or raket_2 == 20:
-   #         pass
-#
- 
-# создаем спрайты
-raket_1 = Player('table-tennis-racket-and-ball-clipart-design-illustration-free-png.png', 50, 250, 75, 75, 10)
-raket_2 = Player('table-tennis-racket-and-ball-clipart-design-illustration-free-png.png', 450, 250, 75, 75, 10)
-ball = Ball('1_1735.png', 250, 250, 40, 40, 7)
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height-80:
+            self.rect.y += self.speed
 
-display.set_caption("Ping Pong")
-window = display.set_mode((500, 500))
-background = transform.scale((500, 500))
-window.fill((0,0,0))
- 
-# Основной цикл игры:
-run = True # флаг сбрасывается кнопкой закрытия окна
-while run:
-    # событие нажатия на кнопку Закрыть
+
+#игровая сцена:
+back = (200, 255, 255) #цвет фона (background)
+win_width = 1000
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+
+#флаги, отвечающие за состояние игры
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+#создания мяча и ракетки 
+ball = GameSprite('1_1735.png', 400, 400, 4, 100, 100)
+racket_l = Player('table-tennis-racket-and-ball-clipart-design-illustration-free-png.png', 10, 200, 4, 25, 150) # 130 - Ш 150 - В
+racket_r = Player('table-tennis-racket-and-ball-clipart-design-illustration-free-png.png', 965, 200, 4, 25, 150)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+setting = font.render('press 1 to exit', True, (0, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+
+while game:
     for e in event.get():
         if e.type == QUIT:
-            run = False
+            game = False
+
+    if finish != True:
+        window.fill(back)
+        racket_l.update_l()
+        racket_r.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > win_height - 100 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(racket_l, ball) or sprite.collide_rect(racket_r, ball):
+            speed_x *= -1
         
- 
-  # сама игра: действия спрайтов, проверка правил игры, перерисовка
-    if not finish:
-        # обновляем фон
-        window.blit(background,(0,0))
+        if ball.rect.x < 0:
+            window.blit(lose1, (win_width//2, win_height//2))
+            finish = True
+            
+        if ball.rect.x > win_width:
+            window.blit(lose2, (win_width//2, win_height//2))
+            finish = True
+            
 
-        # производим движения спрайтов
-        raket_1.update()
-        raket_2.update()
-        ball.update()
+    if finish:
+        window.blit(setting, (win_width//2, win_height//2 + 50,))
+        keys = key.get_pressed()
+        if keys[K_1]:
+            game = False
 
-        # обновляем их в новом местоположении при каждой итерации цикла
-        raket_1.reset()
-        raket_2.reset()
-        ball.reset()
- 
-        # проверка столкновения пули и монстров (и монстр, и пуля при касании исчезают)
-        if ball.rect.x == 20:
-            window.blit(win2, (200, 200))
-        if ball.rect.x == 480:
-            window.blit(win1, (200, 200))
 
-        display.update()
-    # цикл срабатывает каждую 0.05 секунд
-    time.delay(60)
+    ball.reset()
+    racket_l.reset()
+    racket_r.reset()
 
-    #1111111111
-    
+    display.update()
+    clock.tick(FPS)
